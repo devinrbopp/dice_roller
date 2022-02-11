344ddab59875d6f00fc04146a4c53eb325e8ae4e
devinrbopp <devinrbopp@gmail.com>
2022-02-10 21:13:59 -0700

    fixed navigation bar

--- App.tsx
+++ App.tsx
@@ -1,45 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import Die from './components/Die';
 
+
 const Tab = createBottomTabNavigator()
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello this is Devin's app!</Text>
-      <Die />
-      <StatusBar style="auto" />
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName: any;
-
-              if (route.name === 'Die') {
-                iconName = focused
-                  ? 'dice-d20'
-                  : 'dice-d20';
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'ios-list-box' : 'ios-list';
-              }
-
-              // You can return any component that you like here!
-              return <FontAwesome5 name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-          })}
-        >
-          <Tab.Screen name="Die" component={Die} />
+        <Tab.Navigator>
+          <Tab.Screen name="Die" component={Die} options={{
+            tabBarIcon: ({color, size}) => {
+              return <FontAwesome5 name="dice-d20" color={color} size ={size} />
+            }
+          }} />
         </Tab.Navigator>
       </NavigationContainer>
-    </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
--- components/Die.tsx
+++ components/Die.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Die () {
     return(
-        <View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>This is a dice.</Text>
         </View>
     )
