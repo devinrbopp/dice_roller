91850497e2cbb2284f294b128bf410c686ed3512
devinrbopp <devinrbopp@gmail.com>
2022-02-10 20:11:28 -0700

    set up expo application

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Die from './components/Die';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello this is Devin's app!</Text>
+      <Die />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Die.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Die () {
+    return(
+        <View>
+            <Text>This is a dice.</Text>
+        </View>
+    )
+}
