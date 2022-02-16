b117b6f34652755b14d4e3aed8a0e121b4dbb3f3
devinrbopp <devinrbopp@gmail.com>
2022-02-15 21:47:13 -0700

    adjusted font size

--- components/Dice.tsx
+++ components/Dice.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 
 export default function Dice () {
 
-    const [result, setResult] = useState(0)
+    const [result, setResult] = useState('Roll to get started!')
 
     // calculate a value
     const rollDice = (sides: number) => {
@@ -12,7 +12,7 @@ export default function Dice () {
 
     return(
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>{result}</Text>
+            <Text style={{fontSize: 30}}>{result}</Text>
             <Button title='Roll a d20' onPress={() => {setResult(rollDice(20))}} />
         </View>
     )
